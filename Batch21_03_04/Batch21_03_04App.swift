08499fc792d96d106e00e3fc108eed8abccd18c8
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2024-10-31 08:58:38 +0100

    Initial Commit

---
+++ Batch21_03_04/Batch21_03_04App.swift
@@ -0,0 +1,17 @@
+//
+//  Batch21_03_04App.swift
+//  Batch21_03_04
+//
+//  Created by Till Hemmerich on 31.10.24.
+//
+
+import SwiftUI
+
+@main
+struct Batch21_03_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Batch21_03_04/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Batch21_03_04
+//
+//  Created by Till Hemmerich on 31.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
